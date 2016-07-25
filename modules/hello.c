4c95d2e3eac842f45c0d205b373e94a4f9236712
William Oliveira de Lagos <william.lagos@outlook.com>
2016-07-25 19:40:37 +0000

    Added modules initial system

---
+++ modules/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void helloworld()
+{
+  fprintf(stdout,"Hello World!\n");
+}
---
+++ modules/loader.c
@@ -0,0 +1,33 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<dlfcn.h>
+
+int main (int argc, char** argv)
+{
+	char* error;
+	void* handle;
+	void (*hello)();
+	double (*cosine)(double);
+
+	char path[15] = "libhello.so.1";
+	//char path[15] = "/lib/libm.so.6"
+	handle = dlopen(path,RTLD_LAZY);
+
+	if (!handle) {
+		fputs(dlerror(), stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	//cosine = dlsym(handle,"cos");
+	hello = dlsym(handle,"helloworld");
+
+	if ((error = dlerror()) != NULL) {
+		fputs(error, stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	//printf("%f\n", cosine(2.0));
+	//printf("%f\n", cosine(45.0));
+	hello();
+	dlclose(handle);
+}
