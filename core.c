3a16e0efc2af7f886c8846d21d0b07a8e10c7044
William Oliveira de Lagos <william.lagos@outlook.com>
2016-08-22 02:19:11 +0000

    Added backend and app selection

--- core.c
+++ core.c
@@ -6,49 +6,64 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 
-#define APP 1
 #define LOOP 1
-#define BACKEND 0
 #define VERSION "0.1"
 #define MAX_TYPES 2
-#define MAX_MODULES 5
-#define CONFIG_FILE "efforia.cfg"
+#define MAX_MODULES 10
+#define MAX_BUFFER 128
+#define MAX_PATH_SIZE 64
+#define DEFAULT_CONFIGURATION_PATH "efforia.cfg"
 
 typedef struct {
 	char type[25];
 	char* modules[MAX_MODULES];
+	int n_modules;
 } Config;
 
-void config(const char* filename, Config* relations)
+// Read configuration file and return lines buffer to manipulate
+char** config(const char* filename, int* cnt)
 {
-	int count;
-	FILE *stream;
-	char *line = NULL;
-	char *key, *value, *val;
-	size_t len = 0;
-	ssize_t linelen;
-	stream = fopen(filename, "r");
-	if (stream == NULL) exit(EXIT_FAILURE);
-	while ((linelen = getline(&line, &len, stream)) != -1) {
-		if (strchr(line,'[') != NULL) continue;
-		key = strtok(line," =");
-		value = strtok(NULL," =");
-		val = strtok(value,",");
-		count = 0;
-		while (val != NULL) {
-			strcpy(relations->type,key);
-			relations->modules[count] = (char*) malloc(sizeof(val));
-			strcpy(relations->modules[count],val);
-			val = strtok(NULL,",");
-			count++;
-		}
-		relations++;
+	int count = (*cnt);
+	char **lines;
+	char buffer[MAX_BUFFER];
+	FILE *f = fopen(filename, "r");
+	if (f == NULL) return NULL;
+	lines = (char**) calloc(sizeof(char*), MAX_BUFFER);
+	for (count = 0; fgets(buffer, sizeof(buffer), f); count++) {
+		lines[count] = (char*) malloc(sizeof(buffer));
+		strcpy(lines[count], buffer);
 	}
-	free(line);
-	fclose(stream);
+	fclose(f);
+	(*cnt) = count;
+	return lines;
 }
 
-void load(const char* module,const char* func)
+// Read main configuration file and prepares list of modules to be used
+void modules(const char* filename, Config* relations)
+{
+	int count, lin, max;
+	char *key, *val, *value;
+	char **lines = config(filename, &max);
+	if(lines == NULL) exit(EXIT_FAILURE);
+	for (lin = 0; lin < max; lin++) {
+		if (strchr(lines[lin], '[') != NULL) continue;
+		key = strtok(lines[lin], " =");
+		value = strtok(NULL, " =");
+		val = strtok(value, ", ");
+		count = 0;
+		while (val != NULL) {
+			strcpy(relations->type, key);
+			relations->modules[count] = (char*) malloc(sizeof(val));
+			strcpy(relations->modules[count], val);
+			val = strtok(NULL, ", ");
+			count++;
+		}
+		relations->n_modules = count;
+		relations++;
+	}
+}
+
+void start(const char* module, const char* func)
 {
 	char* path;
 	char* error;
@@ -56,15 +71,15 @@ void load(const char* module,const char* func)
 	void (*function)();
 	// Prepare the module string path and open library
 	path = malloc(sizeof(char)*25 + sizeof(module));
-	sprintf(path,"modules/lib%s.so.1",module);
-	handle = dlopen(path,RTLD_LAZY);
+	sprintf(path, "modules/lib%s.so.1", module);
+	handle = dlopen(path, RTLD_LAZY);
 	// Verify if the loading was successful
 	if (!handle) {
 		fputs(dlerror(), stderr);
 		exit(EXIT_FAILURE);
 	}
 	// Load the specified function in the module
-	function = dlsym(handle,func);
+	function = dlsym(handle, func);
 	if ((error = dlerror()) != NULL) {
 		fputs(error, stderr);
 		exit(EXIT_FAILURE);
@@ -72,6 +87,22 @@ void load(const char* module,const char* func)
 	// Execute the function, then close the module
 	function();
 	dlclose(handle);
+}
+
+void schedule(const char* type, const char* module) {
+	char** buffer;
+	int count, max;
+	char path[MAX_PATH_SIZE];
+	sprintf(path, "config/%s.json", module);
+	buffer = config(path, &max);
+	if (buffer != NULL) {
+		printf("Config of %s %s:\n",module,type);
+		for (count = 0; count < max; count++) {
+			printf("%s", buffer[count]);
+		}
+	} else {
+		printf("%s %s without configuration\n",module,type);
+	}
 }
 
 void background()
@@ -86,7 +117,7 @@ void background()
 	}
 	// Exitting parent process
 	if (process_id > 0) {
-		printf("Started Efforia Core %s: %d\n",VERSION,process_id);
+		printf("Started Efforia Core %s: %d\n", VERSION, process_id);
 		exit(EXIT_SUCCESS);
 	}
 	// Prepare the system to start the daemon
@@ -101,30 +132,26 @@ void background()
 int main (int argc, char** argv)
 {
 	int count;
-	Config* relations;
-	Config apps, backends;
-	relations = (Config*) malloc(sizeof(Config) * MAX_TYPES);
-	config(CONFIG_FILE,relations);
-	backends = relations[BACKEND];
-	apps = relations[APP];
-	free(relations);
-	printf("List of %ss:\n",apps.type);
-	for (count = 0; count < MAX_MODULES; count++) {
-		printf("%s\n",apps.modules[count]);
-	}
-	printf("List of %ss:\n",backends.type);
-	for (count = 0; count < MAX_MODULES; count++) {
-		printf("%s\n",backends.modules[count]);
-	}
-	load("hello","helloworld");
+	Config apps, backends, *r;
+	r = (Config*) malloc(sizeof(Config) * MAX_TYPES);
+	modules(DEFAULT_CONFIGURATION_PATH, r);
+	apps = (strstr(r[1].type, "app") != NULL) ? r[1] : r[0];
+	backends = (strstr(r[0].type, "backend") != NULL) ? r[0] : r[1];
+
+	for (count = 0; count < apps.n_modules; count++)
+		schedule(apps.type, apps.modules[count]);
+	for (count = 0; count < backends.n_modules; count++)
+		schedule(backends.type, backends.modules[count]);
+	free(r);
+	load("hello", "helloworld");
 	//background();
 	// Open the file log and start the background service loop
 	FILE* f = NULL;
 	char info[30] = "Logging some information...\n";
-  	f = fopen("efforia.log","w+");
+  	f = fopen("efforia.log", "w+");
   	/*while(LOOP) {
     	sleep(1);
-    	fprintf(f,info);
+    	fprintf(f, info);
     	fflush(f);
   	}*/
   	fclose(f);
